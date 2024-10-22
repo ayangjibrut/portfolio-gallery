9e473f696fe9615c335ffeb06fa9f66f48131bfd
Rizza Marwan <rizzamarwan@icloud.com>
2024-10-22 16:59:12 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+
+hiddenElements.forEach(function(el) {
+    observer.observe(el)
+})
